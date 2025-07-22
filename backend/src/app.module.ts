f2cafa56ce8e70ec3879b6fccab52caae8995153
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-07-22 20:48:31 +0200

    Revert "Merge pull request #3 from SlimDumbledodge/feature-authenticate-users-with-jwt"
    This reverts commit 8007ffdc63ec5a9dcefb4292ea7bf9ec61f2e2f2, reversing
    changes made to a16e9df9ed9b4e8521e10115c8f4d3aaa8643baa.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { ActifsModule } from './actifs/actifs.module';
 
 @Module({
-    imports: [
-        PrismaModule,
-        UsersModule,
-        AuthModule,
-        ActifsModule,
-        ConfigModule.forRoot({
-            isGlobal: true,
-        }),
-        ActifsModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
