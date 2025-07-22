ee818dc128ad680fefb706c0a9927325d038f310
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-07-22 20:36:01 +0200

    feat: imports on app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ActifsModule } from './actifs/actifs.module';
 
 @Module({
-    imports: [PrismaModule, UsersModule],
+    imports: [
+        PrismaModule,
+        UsersModule,
+        AuthModule,
+        ActifsModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        ActifsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
