27171fe883f479af75ac8d71feddfa388dd7ab5a
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-07-25 23:01:47 +0200

    feat: add test

--- backend/src/actifs/actifs.service.spec.ts
+++ backend/src/actifs/actifs.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { ActifsService } from './actifs.service';
 
-describe('AuthService', () => {
-    let service: AuthService;
+describe('ActifsService', () => {
+    let service: ActifsService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [AuthService],
+            providers: [ActifsService],
         }).compile();
 
-        service = module.get<AuthService>(AuthService);
+        service = module.get<ActifsService>(ActifsService);
     });
 
     it('should be defined', () => {
--- backend/src/portfolios/portfolios.service.spec.ts
+++ backend/src/portfolios/portfolios.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { PortfoliosService } from './portfolios.service';
 
-describe('AuthService', () => {
-    let service: AuthService;
+describe('PortfoliosService', () => {
+    let service: PortfoliosService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [AuthService],
+            providers: [PortfoliosService],
         }).compile();
 
-        service = module.get<AuthService>(AuthService);
+        service = module.get<PortfoliosService>(PortfoliosService);
     });
 
     it('should be defined', () => {
