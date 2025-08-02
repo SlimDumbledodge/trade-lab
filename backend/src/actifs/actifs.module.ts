9be8aa8083d4625034a21c57f191d003c4cb1ead
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-02 17:14:22 +0200

    feat: add prodivers & imports

--- backend/src/actifs/actifs.module.ts
+++ backend/src/actifs/actifs.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ActifsService } from './actifs.service';
 import { ActifsController } from './actifs.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { FinnhubModule } from 'src/common/finnhub/finnhub.module';
+import { ActifsCron } from './actifs.cron';
 
 @Module({
     controllers: [ActifsController],
-    providers: [ActifsService],
-    imports: [PrismaModule],
+    providers: [ActifsService, ActifsCron],
+    imports: [PrismaModule, FinnhubModule],
 })
 export class ActifsModule {}
