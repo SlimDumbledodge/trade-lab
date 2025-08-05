476b8d7dcf5da7364945504e59eb022a2307ab9d
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-05 19:16:49 +0200

    feat: import transaction module in portfolio module

--- backend/src/portfolios/portfolios.module.ts
+++ backend/src/portfolios/portfolios.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PortfoliosService } from './portfolios.service';
 import { PortfoliosController } from './portfolios.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { TransactionsModule } from 'src/transactions/transactions.module';
 
 @Module({
     controllers: [PortfoliosController],
     providers: [PortfoliosService],
-    imports: [PrismaModule],
+    imports: [PrismaModule, TransactionsModule],
 })
 export class PortfoliosModule {}
