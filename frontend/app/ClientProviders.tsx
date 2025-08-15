6688dfc46197e9cbfa0b17cd8694066fff476d7a
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-15 22:45:08 +0200

    feat: fix hydratation error because of dark classname

---
+++ frontend/app/ClientProviders.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from '@/components/theme-provider';
+
+export default function ClientProviders({ children }: { children: React.ReactNode }) {
+    return (
+        <SessionProvider>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+        </SessionProvider>
+    );
+}
