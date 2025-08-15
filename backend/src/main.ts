6d426aff3c328e9f99812c3b7a87f43b0fdfc4b7
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-15 22:42:10 +0200

    feat: fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,12 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptors
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Authorization',
+    });
     app.useGlobalFilters(new HttpExceptionFilter());
     app.useGlobalPipes(
         new ValidationPipe({
