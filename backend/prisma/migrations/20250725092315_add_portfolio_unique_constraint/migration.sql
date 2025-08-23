d400f9ada9c375b555e4b310909d69bf7724ab3d
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-23 22:19:03 +0200

    feat: clear migrations

--- backend/prisma/migrations/20250725092315_add_portfolio_unique_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[portfolioId,actifId]` on the table `PortfolioActif` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "PortfolioActif_portfolioId_actifId_key" ON "PortfolioActif"("portfolioId", "actifId");
