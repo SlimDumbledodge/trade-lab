4278de352be839fa5204ea899db7b9d063ee5ca0
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-08-24 20:16:48 +0200

    feat: define call from finnhub

--- backend/src/common/finnhub/finnhub.service.ts
+++ backend/src/common/finnhub/finnhub.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
-import { Actif } from '@prisma/client';
+import { Actif, Company } from '@prisma/client';
 
 type ActifPublic = Omit<Actif, 'id' | 'createdAt' | 'updatedAt' | 'portfolios'>;
+type CompanyPublic = Omit<Company, 'id' | 'createdAt' | 'updatedAt'>;
 
 @Injectable()
 export class FinnhubService {
@@ -12,6 +13,7 @@ export class FinnhubService {
     private readonly baseUrl = 'https://finnhub.io/api/v1/';
     private readonly symbolEndpoint = this.baseUrl + 'search?q=';
     private readonly quoteEndpoint = this.baseUrl + 'quote?symbol=';
+    private readonly companyProfileEndpoint = this.baseUrl + 'stock/profile2?symbol=';
 
     constructor(
         private readonly httpService: HttpService,
@@ -42,10 +44,21 @@ export class FinnhubService {
                 throw new Error(`No quote data found for ${symbol}`);
             }
 
+            const { data: companyProfilInfo } = await firstValueFrom(
+                this.httpService.get(`${this.companyProfileEndpoint}${symbol}${this.getTokenQueryString}`),
+            );
+
+            if (!companyProfilInfo) {
+                throw new Error(`No company profil data found for ${symbol}`);
+            }
+            console.log(companyProfilInfo);
+
             const symbolInfo = dataSymbol.result[0];
 
             return {
                 description: symbolInfo.description ?? 'Description not available',
+                logo: companyProfilInfo.logo,
+                sectorActivity: companyProfilInfo.funnhubIndustry,
                 symbol: symbolInfo.symbol,
                 type: symbolInfo.type ?? 'Unknown',
                 current_price: quoteInfo.c,
@@ -59,6 +72,34 @@ export class FinnhubService {
         } catch (error) {
             console.error(`Failed to get actif info for ${symbol}:`, error);
             throw new Error(`Failed to get actif info for ${symbol}: ${error.message}`);
+        }
+    }
+
+    async getCompanyProfile(symbol: string): Promise<CompanyPublic> {
+        try {
+            const { data } = await firstValueFrom(
+                this.httpService.get(`${this.companyProfileEndpoint}${symbol}${this.getTokenQueryString}`),
+            );
+            console.log(data);
+
+            if (!data) throw new Error(`No profile found for ${symbol}`);
+
+            return {
+                ticker: data.ticker,
+                name: data.name,
+                logo: data.logo,
+                country: data.country,
+                currency: data.currency,
+                exchange: data.exchange,
+                industry: data.finnhubIndustry,
+                marketEntryDate: data.ipo,
+                marketCapitalization: data.marketCapitalization,
+                sharesOutstanding: data.shareOutstanding,
+                webUrl: data.weburl,
+                phone: data.phone,
+            };
+        } catch (err) {
+            throw new Error(`Failed to get company profile for ${symbol}: ${err.message}`);
         }
     }
 }
