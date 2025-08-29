c997a0b2eddf567364ba9b32b59a1ba1ef52eae1
Amaël Rosales <amael.rosales@gmail.com>
2025-08-29 16:44:47 +0200

    Update eslint.config.mjs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -26,8 +26,9 @@ export default tseslint.config(
     },
     {
         rules: {
-            '@typescript-eslint/no-floating-promises': 'warn',
-            '@typescript-eslint/no-unsafe-argument': 'warn',
-        },
+            "@typescript-eslint/no-unsafe-assignment": "off",
+            "@typescript-eslint/no-unsafe-member-access": "off",
+          }
+          
     },
 );
