7ea3aeba105e74251e422ab83e777094d718990d
Amaël Rosales <amael.rosales@gmail.com>
2025-08-29 22:19:03 +0200

    feat: improve style

--- frontend/components/ui/transaction-type-badge.tsx
+++ frontend/components/ui/transaction-type-badge.tsx
@@ -8,10 +8,17 @@ type TransactionTypeBadgeProps = {
 };
 
 export function TransactionTypeBadge({ type }: TransactionTypeBadgeProps) {
-    console.log(type);
+    const isBuy = type === TransactionType.BUY;
+
     return (
-        <Badge className={`${type === TransactionType.BUY ? 'bg-green-700' : 'bg-red-700'} text-white`}>
-            {type === TransactionType.BUY ? 'Achat' : 'Vente'}
+        <Badge
+            className={`shadow-none rounded-full ${
+                isBuy
+                    ? 'bg-red-600/10 dark:bg-red-600/20 hover:bg-red-600/10 text-red-500'
+                    : 'bg-emerald-600/10 dark:bg-emerald-600/20 hover:bg-emerald-600/10 text-emerald-500'
+            }`}
+        >
+            {isBuy ? 'Achat' : 'Vente'}
         </Badge>
     );
 }
