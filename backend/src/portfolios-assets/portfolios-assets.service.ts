e0c37dbff62287bbc9d262a0296dae0d3de18c98
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-11-27 21:18:43 +0100

    (feat): add portfolios-assets module with CRUD service

---
+++ backend/src/portfolios-assets/portfolios-assets.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@nestjs/common"
+import { CreatePortfoliosAssetDto } from "./dto/create-portfolios-asset.dto"
+import { UpdatePortfoliosAssetDto } from "./dto/update-portfolios-asset.dto"
+import { PortfolioAsset, Prisma } from "prisma/generated/client"
+import { PrismaService } from "src/prisma/prisma.service"
+
+@Injectable()
+export class PortfoliosAssetsService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createPortfolioAsset(
+        portfolioId: number,
+        assetId: number,
+        quantity: Prisma.Decimal,
+        buyPrice: Prisma.Decimal,
+    ): Promise<PortfolioAsset> {
+        // On calcule le nouveau prix moyen avant de passer à Prisma
+        const newAverageBuyPrice = await this.calculateNewAverageBuyPrice(portfolioId, assetId, quantity, buyPrice)
+        return this.prisma.portfolioAsset.upsert({
+            where: { portfolioId_assetId: { portfolioId, assetId } },
+            create: {
+                portfolioId,
+                assetId,
+                quantity,
+                averageBuyPrice: buyPrice,
+            },
+            update: {
+                quantity: { increment: quantity },
+                averageBuyPrice: newAverageBuyPrice,
+                updatedAt: new Date(),
+            },
+        })
+    }
+
+    async calculateNewAverageBuyPrice(portfolioId: number, assetId: number, quantity: Prisma.Decimal, buyPrice: Prisma.Decimal) {
+        const existingPortfolioAsset = await this.prisma.portfolioAsset.findUnique({
+            where: { portfolioId_assetId: { portfolioId, assetId } },
+        })
+        if (!existingPortfolioAsset) return buyPrice
+
+        const oldQuantity = existingPortfolioAsset.quantity
+        const oldAverageBuyPrice = existingPortfolioAsset.averageBuyPrice
+
+        const newQuantity = oldQuantity.add(quantity)
+        const totalValue = oldQuantity.mul(oldAverageBuyPrice).add(quantity.mul(buyPrice))
+        const newAverageBuyPrice = totalValue.div(newQuantity)
+
+        return newAverageBuyPrice
+    }
+}
