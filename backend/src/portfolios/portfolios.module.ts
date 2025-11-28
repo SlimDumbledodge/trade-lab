e9c9e4501d9bd9a6e1a2451dd9edd92302d0fa48
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-11-28 21:10:49 +0100

    (feat): add PortfoliosHistoryModule to app and related modules

--- backend/src/portfolios/portfolios.module.ts
+++ backend/src/portfolios/portfolios.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common"
 import { PortfoliosService } from "./portfolios.service"
 import { PortfoliosController } from "./portfolios.controller"
 import { PrismaModule } from "src/prisma/prisma.module"
+import { PortfoliosAssetsModule } from "src/portfolios-assets/portfolios-assets.module"
 
 @Module({
     controllers: [PortfoliosController],
     providers: [PortfoliosService],
-    imports: [PrismaModule],
+    imports: [PrismaModule, PortfoliosAssetsModule],
     exports: [PortfoliosService],
 })
 export class PortfoliosModule {}
--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -4,11 +4,12 @@ import { TransactionsController } from "./transactions.controller"
 import { PrismaModule } from "src/prisma/prisma.module"
 import { PortfoliosModule } from "src/portfolios/portfolios.module"
 import { PortfoliosAssetsModule } from "src/portfolios-assets/portfolios-assets.module"
+import { PortfoliosHistoryModule } from "src/portfolios-history/portfolios-history.module"
 
 @Module({
     controllers: [TransactionsController],
     providers: [TransactionsService],
-    imports: [PrismaModule, PortfoliosModule, PortfoliosAssetsModule],
+    imports: [PrismaModule, PortfoliosModule, PortfoliosAssetsModule, PortfoliosHistoryModule],
     exports: [TransactionsService],
 })
 export class TransactionsModule {}
