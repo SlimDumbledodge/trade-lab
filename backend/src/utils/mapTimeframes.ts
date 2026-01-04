59383d5e397d929e2d839af657a1995fa36a3e14
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-04 17:24:32 +0100

    feat: add utility to map asset price periods to timeframes

---
+++ backend/src/utils/mapTimeframes.ts
@@ -0,0 +1,19 @@
+import { TimeframeEnum } from "src/alpaca/types/alpaca.types"
+import { ASSET_PRICE_PERIOD } from "src/assets-price/types/types"
+
+export function mapTimeframes(period: ASSET_PRICE_PERIOD): TimeframeEnum {
+    switch (period) {
+        case ASSET_PRICE_PERIOD.ONE_DAY:
+            return TimeframeEnum.ONE_MIN
+        case ASSET_PRICE_PERIOD.ONE_WEEK:
+            return TimeframeEnum.ONE_HOUR
+        case ASSET_PRICE_PERIOD.ONE_MONTH:
+            return TimeframeEnum.SIX_HOUR
+        case ASSET_PRICE_PERIOD.SIX_MONTHS:
+            return TimeframeEnum.ONE_DAY
+        case ASSET_PRICE_PERIOD.ONE_YEAR:
+            return TimeframeEnum.ONE_DAY
+        case ASSET_PRICE_PERIOD.FIVE_YEARS:
+            return TimeframeEnum.ONE_WEEK
+    }
+}
