c3ef5cc0564a6f33036bd816cb18d6bb6840c8a7
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-07 10:59:27 +0100

    feat(types): add types for portfolio performance periods

---
+++ backend/src/portfolios-snapshots/types/types.ts
@@ -0,0 +1,14 @@
+import { Prisma } from "prisma/generated/client"
+
+export enum PORTFOLIO_PERFORMANCE_PERIOD {
+    ONE_DAY = "1d",
+    ONE_WEEK = "1s",
+    ONE_MONTH = "1m",
+    SIX_MONTHS = "6m",
+    ONE_YEAR = "1y",
+}
+
+export type PortfolioPerformance = {
+    recordedAt: Date
+    holdingsValue: Prisma.Decimal
+}
