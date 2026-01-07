47f1bb55c5887eeefb75cee18ca813c30e1f8ff4
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-07 21:01:49 +0100

    feat(front): remove dashboard page and update routes to portfolio

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,5 @@
-export { default } from 'next-auth/middleware';
+export { default } from "next-auth/middleware"
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/profile/:path*', '/market/:path*', '/transactions'],
-};
+    matcher: ["/portfolio/:path*", "/profile/:path*", "/market/:path*", "/transactions"],
+}
