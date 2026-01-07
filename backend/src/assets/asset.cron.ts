724e5901f288d90ccfdbc32c00dd20ad1a9afd57
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-07 20:59:15 +0100

    feat(finnhub): add Finnhub integration and asset profile cron job

---
+++ backend/src/assets/asset.cron.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger } from "@nestjs/common"
+import { PrismaService } from "src/prisma/prisma.service"
+import { Cron, CronExpression } from "@nestjs/schedule"
+import { FinnhubService } from "src/finnhub/finnhub.service"
+
+@Injectable()
+export class AssetCron {
+    private readonly logger = new Logger(AssetCron.name)
+
+    constructor(private readonly finnhubService: FinnhubService) {}
+
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+    async updateCompanyProfil() {
+        this.logger.log("updateCompanyProfil : Début ...")
+        await this.finnhubService.updateCompanyProfil()
+        this.logger.log("updateCompanyProfil: Fin ...")
+    }
+}
---
+++ backend/src/finnhub/finnhub.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, Logger } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import axios from "axios"
+import { PrismaService } from "src/prisma/prisma.service"
+
+@Injectable()
+export class FinnhubService {
+    private readonly logger = new Logger(FinnhubService.name)
+    private readonly token: string
+    private readonly baseUrl: string
+    private readonly companyProfilUrl: string
+
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService,
+    ) {
+        this.token = this.configService.get<string>("FINNHUB_API_KEY")!
+        this.baseUrl = this.configService.get<string>("FINNHUB_BASE_URL")!
+        this.companyProfilUrl = this.configService.get<string>("FINNHUB_BASE_COMPANY_PROFIL_URL")!
+    }
+
+    private get getTokenQueryString(): string {
+        return `&token=${this.token}`
+    }
+
+    async updateCompanyProfil() {
+        const assets = await this.prisma.asset.findMany()
+        if (!assets.length) {
+            this.logger.fatal(`updateCompanyProfil : Aucun assets trouver`)
+            throw new Error(`updateCompanyProfil : Aucun assets trouver`)
+        }
+        for (const asset of assets) {
+            try {
+                const response = await axios.get(`${this.baseUrl}${this.companyProfilUrl}${asset.symbol}${this.getTokenQueryString}`)
+                const companyProfil = response.data
+                if (!companyProfil) {
+                    this.logger.warn(`updateCompanyProfil : No profile found for ${asset.symbol}`)
+                    continue
+                }
+
+                await this.prisma.asset.update({
+                    where: { id: asset.id },
+                    data: {
+                        logo: companyProfil.logo,
+                        category: companyProfil.finnhubIndustry,
+                    },
+                })
+
+                this.logger.log(`✅ ${asset.symbol}: profil mis à jour`)
+            } catch (e) {
+                this.logger.error(`updateCompanyProfil ❌ Erreur pour ${asset.symbol}: ${e.message}`, e.stack)
+            }
+        }
+    }
+}
