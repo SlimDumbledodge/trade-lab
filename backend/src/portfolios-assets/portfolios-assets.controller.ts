b485f6236ca132feca983e11bcef22604fa6d81b
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-16 11:05:01 +0100

    feat(portfolios-assets): add endpoint to get portfolio asset by symbol

--- backend/src/portfolios-assets/portfolios-assets.controller.ts
+++ backend/src/portfolios-assets/portfolios-assets.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from "@nestjs/common"
+import { Controller, Get, Param, UseGuards } from "@nestjs/common"
 import { PortfoliosAssetsService } from "./portfolios-assets.service"
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
+import { GetUser } from "src/common/decorators/user.decorator"
 
 @Controller("portfolios-assets")
+@UseGuards(JwtAuthGuard)
 export class PortfoliosAssetsController {
     constructor(private readonly portfoliosAssetsService: PortfoliosAssetsService) {}
+
+    @Get(":symbol")
+    getPortfolioAsset(@Param("symbol") symbol: string, @GetUser("portfolioId") portfolioId: number) {
+        return this.portfoliosAssetsService.getPortfolioAsset(symbol, portfolioId)
+    }
 }
