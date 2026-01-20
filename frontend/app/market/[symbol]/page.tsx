5395a0b29f3c330d5ab16d8a465446ba841330ec
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-20 13:50:21 +0100

    feat(market): refactor market page layout and add position details

--- frontend/app/market/[symbol]/page.tsx
+++ frontend/app/market/[symbol]/page.tsx
@@ -12,14 +12,17 @@ import { PriceChange } from "@/components/ui/price-change"
 import TradeExecutionForm from "@/components/market/TradeExecutionForm"
 import { useAssetPrices } from "@/hooks/useAssetPrices"
 import { useAsset } from "@/hooks/useAsset"
-import { PERIODS } from "@/lib/constants"
+import { ASSET_PRICE_PERIODS } from "@/lib/constants"
+import { PositionDetails } from "@/components/market/PositionDetailsCard"
+import { usePortfolioAsset } from "@/hooks/usePortfolioAsset"
 
 export default function MarketProductDetails() {
     const { data: session } = useSession()
     const params = useParams()
     const symbol = params?.symbol as string
     const [selectedPeriod, setSelectedPeriod] = useState<ASSET_PRICE_PERIOD>(ASSET_PRICE_PERIOD.ONE_DAY)
-    const [performanceData, setPerformanceData] = useState<{ valueEur: number; valuePercent: number }>({
+    const [performanceData, setPerformanceData] = useState<{ totalValue: number; valueEur: number; valuePercent: number }>({
+        totalValue: 0,
         valueEur: 0,
         valuePercent: 0,
     })
@@ -31,11 +34,12 @@ export default function MarketProductDetails() {
 
         if (lastValueRef.current !== differenceEur) {
             lastValueRef.current = differenceEur
-            setPerformanceData({ valueEur: differenceEur, valuePercent: differencePercent })
+            setPerformanceData({ totalValue: currentPrice, valueEur: differenceEur, valuePercent: differencePercent })
         }
     }, [])
 
     const { data: asset, isLoading: assetLoading, error: assetError } = useAsset(symbol, session?.accessToken)
+    const { data: portofolioAsset, isLoading: isPortfolioAssetLoading } = usePortfolioAsset(symbol, session?.accessToken)
 
     const {
         data: assetPrices,
@@ -43,10 +47,9 @@ export default function MarketProductDetails() {
         error: assetPricesError,
     } = useAssetPrices(symbol, selectedPeriod, session?.accessToken ?? undefined)
 
-    if (assetLoading || assetPricesLoading) return <p>Chargement...</p>
+    if (assetLoading || assetPricesLoading || isPortfolioAssetLoading) return <p>Chargement...</p>
     if (assetError || assetPricesError) return <p className="text-red-600">{assetError?.message || assetPricesError?.message}</p>
     if (!asset || !assetPrices) return <p>Introuvable</p>
-    console.log(assetPrices)
 
     const formatAssetPrices: PricePoint[] = assetPrices.map((price) => {
         return {
@@ -54,50 +57,57 @@ export default function MarketProductDetails() {
             closingPrice: price.closingPrice,
         }
     })
+    const lastClosingPrice = formatAssetPrices[formatAssetPrices.length - 1]
 
     return (
         <HomeLayout headerTitle={"Marché"}>
-            <div className="flex flex-col gap-6">
-                {/* En-tête avec logo et informations */}
-                <div className="flex items-start gap-4">
-                    <Image className="rounded-xl shadow" src={asset.logo} alt={asset.symbol} width={48} height={48} />
-                    <div className="flex flex-col gap-1">
-                        <h1 className="text-3xl font-bold">{asset.name}</h1>
-                        <p className="text-2xl font-bold">{Number(asset.lastPrice).toFixed(2)} €</p>
-                        <div className="flex gap-2">
-                            <PriceChange value={performanceData.valueEur} />
-                            <span
-                                className={`font-semibold text-s ${performanceData.valuePercent > 0 ? "text-green-500" : performanceData.valuePercent < 0 ? "text-red-500" : "text-muted-foreground"}`}
-                            >
-                                ({performanceData.valuePercent > 0 ? "+" : ""}
-                                {performanceData.valuePercent.toFixed(2)}%)
-                            </span>
+            <div className="flex flex-col lg:flex-row gap-6">
+                {/* Partie gauche: En-tête, boutons de période et graphique */}
+                <div className="flex-1 flex flex-col gap-6">
+                    {/* En-tête avec logo et informations */}
+                    <div className="flex items-start gap-4">
+                        <Image className="rounded-xl shadow" src={asset.logo} alt={asset.symbol} width={48} height={48} />
+                        <div className="flex flex-col gap-1">
+                            <h1 className="text-3xl font-bold">{asset.name}</h1>
+                            <p className="text-2xl font-bold">
+                                {Number(performanceData.totalValue ?? lastClosingPrice.closingPrice).toFixed(2)} €
+                            </p>
+                            <div className="flex gap-2">
+                                <PriceChange value={performanceData.valueEur} />
+                                <span
+                                    className={`font-semibold text-s ${performanceData.valuePercent > 0 ? "text-green-500" : performanceData.valuePercent < 0 ? "text-red-500" : "text-muted-foreground"}`}
+                                >
+                                    ({performanceData.valuePercent > 0 ? "+" : ""}
+                                    {performanceData.valuePercent.toFixed(2)}%)
+                                </span>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                {/* Boutons de période */}
-                <div className="flex gap-2">
-                    {PERIODS.map((period) => (
-                        <Button
-                            variant={selectedPeriod === period.value ? "link" : "ghost"}
-                            key={period.value}
-                            onClick={() => setSelectedPeriod(period.value)}
-                            className={`text-xs rounded font-bold`}
-                        >
-                            {period.label}
-                        </Button>
-                    ))}
-                </div>
-
-                {/* Graphique et formulaire */}
-                <div className="flex gap-6">
-                    <div className="flex-1">
-                        <AssetPriceChart data={formatAssetPrices} handlePerformanceData={handlePerformanceData} />
+                    {/* Boutons de période */}
+                    <div className="flex self-end gap-2">
+                        {ASSET_PRICE_PERIODS.map((period) => (
+                            <Button
+                                variant={selectedPeriod === period.value ? "link" : "ghost"}
+                                key={period.value}
+                                onClick={() => setSelectedPeriod(period.value)}
+                                className={`text-xs rounded font-bold`}
+                            >
+                                {period.label}
+                            </Button>
+                        ))}
                     </div>
-                    <div>
+
+                    {/* Graphique */}
+                    <AssetPriceChart data={formatAssetPrices} handlePerformanceData={handlePerformanceData} />
+                </div>
+
+                {/* Partie droite: Formulaire et Position */}
+                <div className="flex flex-col sm:flex-row lg:flex-col gap-6 w-full lg:w-[350px]">
+                    <div className="flex-1 sm:flex-1 lg:flex-none">
                         <TradeExecutionForm />
                     </div>
+                    <div className="flex-1 sm:flex-1 lg:flex-none">{portofolioAsset?.assetId && <PositionDetails />}</div>
                 </div>
             </div>
         </HomeLayout>
