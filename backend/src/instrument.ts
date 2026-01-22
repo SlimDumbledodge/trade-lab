1804a8159abff8ce2c3b4a01e648e005b088b8e7
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-22 11:47:20 +0100

    feat(sentry): add Sentry instrumentation for profiling

---
+++ backend/src/instrument.ts
@@ -0,0 +1,15 @@
+// Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
+import * as Sentry from "@sentry/nestjs"
+import { nodeProfilingIntegration } from "@sentry/profiling-node"
+
+Sentry.init({
+    dsn: "https://7176c08f43209136e93fd5f89f8c9ee1@o4510751050301440.ingest.de.sentry.io/4510751054168144",
+    integrations: [nodeProfilingIntegration()],
+    environment: process.env.NODE_ENV || "development",
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+    profileSessionSampleRate: 1.0,
+    // Trace lifecycle automatically enables profiling during active traces
+    profileLifecycle: "trace",
+})
