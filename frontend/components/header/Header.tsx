ad95f430fe1e7898c4afb8d9de5ec4dfb18d58fa
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2026-01-23 17:16:12 +0100

    feat(components): add dynamic market status badge to header

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -3,20 +3,19 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { ModeToggle } from "../theme/ModeToggle"
-import { ReactNode } from "react"
-import { usePathname } from "next/navigation"
+import { ReactNode, Suspense } from "react"
+import MarketOpenBadge from "../ui/market-status-badge"
 
 export function SiteHeader({ headerTitle }: { headerTitle: ReactNode }) {
-    const pathname = usePathname()
-
     return (
         <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
             <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-                <h1 className="text-base font-medium">
-                    {headerTitle ?? pathname.replace("/", "").charAt(0).toUpperCase() + pathname.slice(2)}
-                </h1>
+                <Suspense fallback={<p>Chargement du status du marché...</p>}>
+                    <MarketOpenBadge />
+                </Suspense>
+
                 <div className="ml-auto flex items-center gap-4">
                     <ModeToggle />
                 </div>
--- frontend/components/ui/market-status-badge.tsx
+++ frontend/components/ui/market-status-badge.tsx
@@ -1,36 +1,35 @@
 "use client"
-
 import { Badge } from "@/components/ui/badge"
-import { cn } from "@/lib/utils"
+import { useMarketStatus } from "@/hooks/useMarketStatus"
+import moment from "moment"
+import { useSession } from "next-auth/react"
 
-type MarketStatusBadgeProps = {
-    isOpen: boolean
-    className?: string
-}
+const MarketStatusBadge = () => {
+    const { data: session } = useSession()
+    const { data: marketStatus, error } = useMarketStatus(session?.accessToken)
 
-export function MarketStatusBadge({ isOpen, className }: MarketStatusBadgeProps) {
-    return (
-        <Badge
-            className={cn(
-                "relative flex items-center gap-2 px-3 py-1 rounded-full shadow-none",
-                isOpen ? "bg-emerald-600/10 text-emerald-500" : "bg-red-600/10 text-red-500",
-                className,
-            )}
-        >
-            {/* Container du statut avec le pulse */}
-            <span className="relative flex h-2 w-2">
-                {/* Cercle qui pulse */}
-                <span
-                    className={cn(
-                        "absolute inline-flex h-full w-full rounded-full opacity-75 animate-ping",
-                        isOpen ? "bg-emerald-400" : "bg-red-400",
-                    )}
-                />
-                {/* Pastille centrale fixe */}
-                <span className={cn("relative inline-flex h-2 w-2 rounded-full", isOpen ? "bg-emerald-500" : "bg-red-500")} />
-            </span>
+    if (error || !marketStatus)
+        return (
+            <Badge className="border-none bg-destructive/10 text-destructive">Impossible de récupérer le statut du marché</Badge>
+        )
+    const { isOpen, todayMarketInfos, nextDayMarketInfos } = marketStatus
 
-            {isOpen ? "Marché ouvert" : "Marché fermé"}
+    const formatTime = (date: Date | string | null | undefined) => (date ? moment(date).format("HH[h]mm") : "--:--")
+    const todayOpen = formatTime(todayMarketInfos?.openTime)
+    const todayClose = formatTime(todayMarketInfos?.closeTime)
+    const nextOpenDate = moment(nextDayMarketInfos?.openTime).format("dddd DD/MM")
+    const nextOpenTime = formatTime(nextDayMarketInfos?.openTime)
+    return isOpen ? (
+        <Badge className="border-none bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 focus-visible:outline-none [a&]:hover:bg-green-600/5 dark:[a&]:hover:bg-green-400/5">
+            <span className="size-1.5 rounded-full bg-green-600 dark:bg-green-400" aria-hidden="true" />
+            Marché ouvert de {todayOpen} à {todayClose}
+        </Badge>
+    ) : (
+        <Badge className="border-none bg-destructive/10 text-destructive focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 focus-visible:outline-none [a&]:hover:bg-destructive/5">
+            <span className="size-1.5 rounded-full bg-destructive" aria-hidden="true" />
+            Marché fermé - Ouvre le {nextOpenDate} à {nextOpenTime}
         </Badge>
     )
 }
+
+export default MarketStatusBadge
